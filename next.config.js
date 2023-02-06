8af27e3e12a242b241385f1236095328114bb477
MeshachArinze <meshacharinze@gmail.com>
2023-02-06 15:40:22 +0100

    refactor to github

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
+  compress: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
+  trailingSlash: true,
   experimental: {
     appDir: true,
   },
-}
+};
 
 module.exports = nextConfig
