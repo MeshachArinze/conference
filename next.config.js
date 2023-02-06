820af92ab9bd113a197c693b06fd3b5c5a9678c1
MeshachArinze <meshacharinze@gmail.com>
2023-02-06 15:56:37 +0100

    refactor to repo

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/about",
+        headers: [
+          {
+            key: "x-custom-header",
+            value: "my custom header value",
+          },
+          {
+            key: "x-another-custom-header",
+            value: "my other custom header value",
+          },
+        ],
+      },
+    ];
+  },
   pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
   compress: false,
   eslint: {
