498a8dd842ef1d26a80577f78b333abb5b7124c1
MeshachArinze <meshacharinze@gmail.com>
2023-02-06 21:43:23 +0100

    commit to master

--- src/pages/components/App.js
+++ src/pages/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Home from "../components/Home";
 import Speakers from "../components/Speakers";
+import Custom404 from "./404";
 
 const App = ({ pageName }) => {
-  if (pageName === "Home") return <Home />;
-  if (pageName === "Speakers") return <Speakers />;
-  return <div>Not Found</div>;
+  if (pageName === "Home") {
+    return <Home />
+  } else if (pageName === "Speakers") {
+    return <Speakers />;
+  } else {
+    return <Custom404 />;
+  }
 };
 
 export default App;
